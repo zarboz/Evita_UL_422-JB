65c7d4077b88fb1674f676c1e82f01c14e4bddbf
flar2 <asegaert@gmail.com>
2013-05-01 17:07:24 -0400

    fix hsic wakelock https://www.codeaurora.org/gitweb/quic/la/?p=kernel/msm.git;a=commit;h=41d004c4eaa5565cb8d9113862c3cef1e51be648 and https://www.codeaurora.org/gitweb/quic/la/?p=kernel/msm.git;a=commit;h=6f199aa800253a69e5679d1057823039f1bd65c9
    Signed-off-by: Bryan Smith <zarboz@gmail.com>

--- drivers/usb/core/driver.c
+++ drivers/usb/core/driver.c
@@ -1106,6 +1106,14 @@ int usb_resume(struct device *dev, pm_message_t msg)
 	struct usb_device	*udev = to_usb_device(dev);
 	int			status;
 
+        /*
+         * Some buses would like to keep their devices in suspend
+         * state after system resume.  Their resume happen when
+         * a remote wakeup is detected or interface driver start
+         * I/O.
+         */
+	if (udev->bus->skip_resume)
+               return 0;
 	pm_runtime_get_sync(dev->parent);
 	status = usb_resume_both(udev, msg);
 	if (status == 0) {
