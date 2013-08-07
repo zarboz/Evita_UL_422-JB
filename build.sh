adc3180c74e12762d7ccc304c826bcbcf5c7002e
Bryan Smith <zarboz@gmail.com>
2013-08-07 15:55:01 -0600

    fixing so buildbot can work
    Signed-off-by: Bryan Smith <zarboz@gmail.com>

--- build.sh
+++ build.sh
@@ -3,13 +3,9 @@ rm output/system/lib/modules/*.ko
 rm output/kernel/zImage
 make CROSS_COMPILE=android-toolchain-eabi/bin/arm-eabi- clean
 make CROSS_COMPILE=android-toolchain-eabi/bin/arm-eabi- mrproper
-make CROSS_COMPILE=android-toolchain-eabi/bin/arm-eabi- operaul_defconfig
+make CROSS_COMPILE=android-toolchain-eabi/bin/arm-eabi- elite_defconfig
 make CROSS_COMPILE=android-toolchain-eabi/bin/arm-eabi- -j9
 cp arch/arm/boot/zImage output/kernel/zImage
 find . -name '*.ko' -exec cp {} output/system/lib/modules/  \;
-cd output/system/app/
-rm JmzSettings.apk
-wget https://dl.dropboxusercontent.com/u/28491940/JmzSettings.apk
-cd ../..
 NOW=$(date +"%m-%d-%y")
-zip -r JmzMyst_Kernel-"$NOW".zip *
+zip -r test_Kernel-"$NOW".zip *
